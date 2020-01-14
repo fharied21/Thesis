380ef2090b7b1fa77d9510e9bec2c2507379818e
minatorisato <36133909+minatorisato@users.noreply.github.com>
2020-01-14 20:46:57 +0700

    stress test

--- test.php
+++ test.php
@@ -8,11 +8,30 @@ curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
 $result = curl_exec($curl);
 $guitar = array();
 
-preg_match_all('!<h2 class="woocommerce-loop-product__title">(.*?)<\/h2>!',$result,$match);
-$guitar['name']=$match[1];
-
-
 preg_match_all('!<span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">Rp<\/span>&nbsp;(.*?)<\/span>!',$result,$match);
-$guitar['price']=$match[1];
-print_r($guitar['name'][1]);
+$list['price']=$match[1];
+
+
+preg_match_all('!<a href="(.*?)" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">!',$result,$match);
+$list['link']=$match[1];
+$guitar = [];
+$counter = 0;
+curl_close($curl);
+foreach($list['link'] as $url){
+    $curl = curl_init();
+
+    curl_setopt($curl,CURLOPT_URL,$url);
+    curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+        
+    $result = curl_exec($curl);
+    $object = new stdClass();
+    preg_match_all('!<h1 class="product_title entry-title">(.*?)<\/h1>!',$result,$match);
+    $object->name = $match[1][0];
+    $object->price = $list['price'][$counter];
+    
+    print_r($object->name.' '.$object->price.'</br>');
+    array_push($guitar,$object);
+    $counter++;
+    curl_close($curl);
+}
 ?>
